9b1fc253ac98be5614e48dae3bd87866ac11a1b1
Steven C. Gonzalez <stevenconnorg@gmail.com>
2018-04-20 12:45:03 -0500

    Update joinCalc.py

--- joinCalc.py
+++ joinCalc.py
@@ -18,14 +18,15 @@ IdFld =arcpy.GetParameterAsText(4)
 updateFld =arcpy.GetParameterAsText(5)
 whereClause=arcpy.GetParameterAsText(6)
 checkedLeftstrip = arcpy.GetParameterAsText(7)
+checkedRemoveBlanks = arcpy.GetParameterAsText(8)
 
 
 #join key field
-sourceRPSUID =arcpy.GetParameterAsText(8)
+sourceRPSUID =arcpy.GetParameterAsText(9)
 
 
 #join key field
-updateRPSUID =arcpy.GetParameterAsText(9)
+updateRPSUID =arcpy.GetParameterAsText(10)
 
 # remove leading zeros from destination join id field?
 if checkedLeftstrip:
@@ -34,12 +35,17 @@ if checkedLeftstrip:
                 row=[i.lstrip('0') for i in row]
                 cursor.updateRow(row)
                 
-                
+if checkedRemoveBlanks:
+    with arcpy.da.UpdateCursor(pointFc,[IdFld]) as cursor:
+        for row in cursor:
+            row=[i.strip() if i is not None else None for i in row]
+            cursor.updateRow(row)
+        
 def unique_values(table , field):
     with arcpy.da.SearchCursor(table, [field]) as cursor:
         return sorted({str(row[0]) for row in cursor})
 
-myRPSUIDs = unique_values(pointFc, updateRPSUID)
+myRPSUIDs = unique_values(pointFc, str(updateRPSUID))
 
 for RPSUID in myRPSUIDs:
     # for RPSUID in myRPSUIDs:
@@ -50,21 +56,19 @@ for RPSUID in myRPSUIDs:
         arcpy.Delete_management(tbl)
     arcpy.TableSelect_analysis(joinFc,tbl, where_clause=sourceRPSUID+' =' +RPSUID+" and "+joinValFld+ " "+whereClause)
     
-    
-    	#create dictionary
-    	#Key: join field
-    	#Value: field with value to be transferred
-    valueDi = dict ([(key, val) for key, val in
+	#create dictionary
+	#Key: join field
+	#Value: field with value to be transferred
+    valueDi = dict([(key, val) for key, val in
     					 arcpy.da.SearchCursor
     					 (tbl, [joinIdFld, joinValFld])])
     
-    
-    	#update feature class
-    
+    valueDi =  {k:int(v) for k, v in valueDi.iteritems()}
+    #update feature class
     with arcpy.da.UpdateCursor (pointFc, [updateFld, IdFld],where_clause=updateRPSUID+" = '"+RPSUID+"'") as cursor:
         for update, key in cursor:
     			#skip if key value is not in dictionary
-            if not key in valueDi:
+            if not str(key) in valueDi:
                 continue
     			#create row tuple
             row = (valueDi [key], key)
@@ -75,5 +79,4 @@ for RPSUID in myRPSUIDs:
         
     del cursor
     arcpy.Delete_management(tbl)
-
 
